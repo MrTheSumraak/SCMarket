74d8364fc63dbb4f90d5309dbbcce539431fb195
DreamHarakiri <derek.gaming744@gmail.com>
2025-11-29 22:48:13 +0300

    Test fetch on demo api

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        proxy: {
+            // Proxy /api/* -> https://dapi.stalcraft.net/*
+            '/api': {
+                target: 'https://dapi.stalcraft.net',
+                changeOrigin: true,
+                secure: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
+    }
+});
